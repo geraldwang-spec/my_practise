82fe8bcfec1e943c5eb163d28c559d08c182ad29
gerald <g123erald456@gmail.com>
2026-02-12 00:10:12 +0800

    add decorateors example

--- pythonP/src/pythonP/mytest/lambdatest.py
+++ pythonP/src/pythonP/mytest/lambdatest.py
@@ -1,5 +1,48 @@
-from typing import Callable
+from typing import Callable, ParamSpec, TypeVar
 
+P = ParamSpec("P")
+R = TypeVar("R")
+
+def some_decorator(f: Callable[P, R])->Callable[P,R]:
+    def wraps(*args: P.args, **kwargs: P.kwargs) -> R:
+        """this is decorator of wraps"""
+        print(f"--- 裝飾器啟動：準備呼叫 {f.__name__} ---")
+        return f(*args, **kwargs)
+    return wraps
+
+@some_decorator
+def decorators_function(x: str) -> None:
+    """ths is test decorator function"""
+    print(f"execution this function: print {x}")
+
+@some_decorator
+def decorators_function1(x:int)->None:
+    """this is decorated function1 test int """
+    print(f"print x = {x}")
+
+def decorators_example()->None:
+    print("1. 呼叫函數的結果：")
+    decorators_function("Hello")
+    
+    print("\n2. 檢查函數的名字 (__name__)：")
+    # 如果沒有被掉包，應該印出 "decorated_function"
+    # 但因為被裝飾了，它現在會印出裝飾器內部的 "wraps"
+    print(f"目前的函數名稱是: {decorators_function.__name__}")
+    
+    print("\n3. 檢查函數的說明文件 (__doc__)：")
+    # 原本的註解也會消失，因為現在的名字指向 wraps
+    print(f"目前的說明文件是: {decorators_function.__doc__}")
+
+    decorators_function1(10)
+    
+    print("\n2. 檢查函數的名字 (__name__)：")
+    # 如果沒有被掉包，應該印出 "decorated_function"
+    # 但因為被裝飾了，它現在會印出裝飾器內部的 "wraps"
+    print(f"目前的函數名稱是: {decorators_function1.__name__}")
+    
+    print("\n3. 檢查函數的說明文件 (__doc__)：")
+    # 原本的註解也會消失，因為現在的名字指向 wraps
+    print(f"目前的說明文件是: {decorators_function1.__doc__}")
 
 def basic_test()->None:
     func:Callable[[int], int] = lambda x: x+1
@@ -22,6 +65,7 @@ def basic_test()->None:
 def lambda_example()-> None:
     print("lambda_example")
     # basic_test()
+    decorators_example()
 
 
 
