5d7548292c600ea701f2fe95e584d5a79e092081
qoo <qoo@tt.com>
2026-02-11 18:00:34 +0800

    python test

--- pythonP/src/pythonP/mytest/lambdatest.py
+++ pythonP/src/pythonP/mytest/lambdatest.py
@@ -1,6 +1,7 @@
 from typing import Callable
 
-def basic_test():
+
+def basic_test()->None:
     func:Callable[[int], int] = lambda x: x+1
     _c:int = func(2)
     print(f"_c = {_c}")
@@ -11,10 +12,16 @@ def basic_test():
     func2:Callable[[int,int],int] = lambda x,y: x+y
     print(f"result = {func2(2,3)}")
 
+    func3: Callable[[int, Callable[[int],int]], int] = lambda x, func: x + func(x)
+    print(f"func3 = {func3(5, lambda x:  x * 2)}")
+
+    func4: Callable[[int],float] = lambda x: x/1
+    print(f"func4 = {func4(2)}")
+
 
 def lambda_example()-> None:
     print("lambda_example")
-    basic_test()
+    # basic_test()
 
 
 
