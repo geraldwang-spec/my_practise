0c10e12d728c1a9b510650aba28246fd02022937
qoo <qoo@tt.com>
2026-02-11 13:04:23 +0800

    modify lambda example

--- pythonP/src/pythonP/mytest/lambdatest.py
+++ pythonP/src/pythonP/mytest/lambdatest.py
@@ -1,3 +1,21 @@
+from typing import Callable
+
+def basic_test():
+    func:Callable[[int], int] = lambda x: x+1
+    _c:int = func(2)
+    print(f"_c = {_c}")
+
+    func1:Callable[[], None] = lambda : print("QOO")
+    func1()
+
+    func2:Callable[[int,int],int] = lambda x,y: x+y
+    print(f"result = {func2(2,3)}")
+
+
 def lambda_example()-> None:
     print("lambda_example")
+    basic_test()
+
+
+
 
