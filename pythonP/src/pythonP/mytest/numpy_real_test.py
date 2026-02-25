229d7e8a783ff4c23cc63940fde01d38415cd259
gerald <g123erald456@gmail.com>
2026-02-25 23:25:13 +0800

    add array practice Array_04/05

--- pythonP/src/pythonP/mytest/numpy_real_test.py
+++ pythonP/src/pythonP/mytest/numpy_real_test.py
@@ -1,6 +1,7 @@
 from itertools import zip_longest
 import numpy as np
 from numpy.typing import NDArray
+# from typing import cast
 
 class array_practice:
     def OneArray_01(self)->None:
@@ -27,9 +28,59 @@ class array_practice:
         for i in range(max_len):
             print(f"odd/even= {odd_padded[i]}, {even_padded[i]}")
 
+    def OneArray_04(self)->None:
+        box: NDArray[np.int_] = np.random.randint(0, 20, 30, dtype=np.int_)
+        temp_result: list[int] = []
+        print(f"box = {box}")
+
+        for i in range(len(box)):
+            if box[i] == 8:
+                temp_result.append(i)
+
+        result: NDArray[np.int_] = np.array(temp_result, dtype=np.int_)
+
+        if len(result) == 0:
+            print(f"this box has not 8")
+        else:
+            print(f"result = {result}")
+
+    def OneArray_04_adv(self)->None:
+        box: NDArray[np.int_] = np.random.randint(0, 20, 30, dtype=np.int_)
+        print(f"box = {box}")
+
+        # result: NDArray[np.int_] = np.where(cast(NDArray[np.bool_], box == 8))[0] # must from typing import cast
+        result: NDArray[np.int_] = np.where(np.equal(box, 8))[0]
+        if result.size == 0:
+            print(f"this box has not 8")
+        else:
+            print(f"result = {result}")
+
+    def OneArray_05(self)->None:
+        array: list[int] = np.random.randint(0, 30, 10, dtype=np.int_).tolist()
+        # array: list[int] = [10,8,9,3,4,5,6,7,1, 1]
+        print(f"array = {array}")
+        min_vol:int = min(array)
+        print(f"min = {min_vol}")
+        # result: list[int] = np.where(np.equal(array, min_vol))[0].tolist()
+        result: list[int] = [i for i, x in enumerate(array) if x == min_vol]
+        print(f"result = {result}")
+        for item in range(len(result)):
+            if result[item] == len(array) - 1 - item:
+                continue
+
+            array[result[item]] ^= array[-1 * item - 1]
+            array[-1 * item - 1] ^= array[result[item]]
+            array[result[item]] ^= array[-1 * item - 1]
+
+        print(f"result = {array}")
+
+
 
 def numpy_real_example()->None:
     print("umpy real example")
     arrayP:array_practice = array_practice()
     # arrayP.OneArray_01()
-    arrayP.OneArray_03()
+    # arrayP.OneArray_03()
+    # arrayP.OneArray_04()
+    # arrayP.OneArray_04_adv()
+    arrayP.OneArray_05()
