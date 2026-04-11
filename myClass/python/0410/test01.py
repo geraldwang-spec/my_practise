71a3fb9c558beb79a7e98fd60f27c77a4c041d2c
gerald <g123erald456@gmail.com>
2026-04-11 16:53:33 +0800

    adding new class of python

---
+++ myClass/python/0410/test01.py
@@ -0,0 +1,12 @@
+     
+def test01()->None:
+    print("Gerald Wang")
+    print("It is very good class")
+
+    x:int = 0
+    for i in range(11):
+        x = x + i
+        print("total = ", x)
+
+if __name__ == "__main__":
+    test01()
