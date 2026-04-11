92a3997f67c46590a040e35e973b57734b6e4dec
gerald <g123erald456@gmail.com>
2026-04-11 16:54:03 +0800

    adding new class of python

--- myClass/python/0410/test01.py
+++ myClass/python/0410/test01.py
@@ -1,4 +1,12 @@
-     
+
+def test02()->None:
+    high = eval(input("請輸入身高(cm):")) / 100
+    weight = eval(input("請輸入體重(kg): "))
+    
+    bmi = weight/(high ** 2)
+    print(f"身高:{high} cm | 體重:{weight} kg | BMI: {bmi}")
+
+
 def test01()->None:
     print("Gerald Wang")
     print("It is very good class")
@@ -9,4 +17,5 @@ def test01()->None:
         print("total = ", x)
 
 if __name__ == "__main__":
-    test01()
+    # test01()
+    test02()
